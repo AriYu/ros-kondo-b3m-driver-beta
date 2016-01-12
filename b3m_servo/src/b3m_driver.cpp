a707644c12a3bcbf974d06a59c8dd2bf3d68e93c
Arita Yuta <k104009y@mail.kyutech.jp>
2016-01-12 23:07:40 +0900

    [fix] subscribeが上手くいっていなかったのを修正

--- b3m_servo/src/b3m_driver.cpp
+++ b3m_servo/src/b3m_driver.cpp
@@ -17,14 +17,23 @@ class b3m_servo_driver
     servo_.b3mNormalPosModeSet(&port_, servo_id_);
     servo_.b3mTrajectoryModeSet(&port_, servo_id_, TRAJECTORY_EVEN_MODE);
     servo_.b3mGainParamSet(&port_, servo_id_, 0x00);
-   
-    joint_angle_sub_ = nh_.subscribe<sensor_msgs::JointState>("/data", 1, boost::bind(&b3m_servo_driver::joint_cb, this, _1));
+    joint_angle_sub_ = nh_.subscribe<sensor_msgs::JointState>("/joint_states", 1, boost::bind(&b3m_servo_driver::joint_cb, this, _1));
   }
   void joint_cb(const sensor_msgs::JointStateConstPtr& joint_state)
   {
-    short angle = joint_state->position[0];
+    short angle = (short)(joint_state->position[0] * 100);
     short target_time = 0;
+    ROS_INFO("Angle : %d\n",angle);
     servo_.b3mSetPosition(&port_, servo_id_, angle, target_time);
+    usleep(1000);
+  }
+  void run()
+  {
+    while(nh_.ok())
+	{
+	  ros::spinOnce();
+	  rate_.sleep();
+    }
   }
   ~b3m_servo_driver()
   {
@@ -45,9 +54,9 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "b3m_servo_node");
   ros::NodeHandle nh;
   
-  b3m_servo_driver(nh, "/dev/ttyUSB0", B115200);
+  b3m_servo_driver  driver(nh, "/dev/ttyUSB0", B115200);
+  driver.run();
 
-  ros::spin();
 
   return 0;
 }
